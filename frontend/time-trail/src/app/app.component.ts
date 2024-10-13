4ded6debdc25a6ac12acec52abdefdd181e0b616
Mike Lindner <mlindner9@yahoo.com>
2024-10-13 14:10:04 -0400

    Add toolbar.

--- frontend/time-trail/src/app/app.component.ts
+++ frontend/time-trail/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, ToolbarComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'time-trail';
+	title = 'time-trail';
 }
---
+++ frontend/time-trail/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+	selector: 'app-toolbar',
+	standalone: true,
+	imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+	templateUrl: './toolbar.component.html',
+	styleUrl: './toolbar.component.css'
+})
+export class ToolbarComponent {
+
+}
